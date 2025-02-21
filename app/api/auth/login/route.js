e756a49f0a932948a709fa1e1fd55af7d1ffce24
Shanta Dey <shantadey98@gmail.com>
2025-02-21 13:35:37 +0600

    3rd commit

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -39,7 +39,7 @@ export async function POST(req) {
     }
 
     // Generate JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ userId: user._id }, "secret123", {
       expiresIn: "1h",
     });
 
